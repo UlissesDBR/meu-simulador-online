27167997057b557734fbe8aed4d160829be241e7
Ulisses Dias da Silva <sou.ulissess@gmail.com>
2025-09-27 02:03:25 -0300

    Revert to default Next.js config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Adiciona a instrução para exportar como site estático
-  output: 'export',
-
-  // Necessário para o componente <Image> funcionar no modo estático
-  images: {
-    unoptimized: true,
-  },
+  // Não precisamos mais de nenhuma configuração especial aqui
 };
 
 module.exports = nextConfig;
